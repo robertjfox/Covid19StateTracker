5154d1c9c93ac73eb31ad8fd39e904e6c053e7ad
Robert Fox <robert@foxs.com>
2020-05-25 09:42:44 -0400

    Updates

---
+++ precache-manifest.ca278659d97553b4fff3bcb88c63ff71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75192a5afdf2b429071ea62203a6f91a",
+    "url": "/Covid19StateTracker/index.html"
+  },
+  {
+    "revision": "3b7386647accd409fab5",
+    "url": "/Covid19StateTracker/static/css/main.87a06867.chunk.css"
+  },
+  {
+    "revision": "2f52e4ee82b5f86ba543",
+    "url": "/Covid19StateTracker/static/js/2.193bc5ef.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/Covid19StateTracker/static/js/2.193bc5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7386647accd409fab5",
+    "url": "/Covid19StateTracker/static/js/main.922d35b8.chunk.js"
+  },
+  {
+    "revision": "2e32196aff563317d9ca",
+    "url": "/Covid19StateTracker/static/js/runtime-main.f7bedd64.js"
+  }
+]);
